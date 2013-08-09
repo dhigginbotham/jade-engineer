22199f558434ac166103cbb158e588d8a5ed999a
David Higginbotham <davehigginbotham@gmail.com>
2013-08-08 22:51:39 -0600

    refactored make and added a new example

--- example/app.js
+++ example/app.js
@@ -3,34 +3,70 @@ var app = express();
 
 var server = require('http').createServer(app);
 
+// include some data, an easy replacement would be
+// to grab this from mongodb or something similar.
 var data = require('./data.json');
+
+// require jade-engineer
 var engineer = require('../lib');
 
 var path = require('path');
 
+//1337 port is 1337
 app.set('port', 1337);
+
+// set our view and view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 var exampleOptions = {
-  keys: ['name', 'phone', 'date', 'email'],
-  titles: ['Name', 'Phone', 'Date', 'Email Address']
-}
+  fields: [
+    {key: 'name', title: 'Name'},
+    {key: 'phone', title: 'Phone'},
+    {key: 'date', title: 'Date'},
+    {key: 'email', title: 'Email Address'}
+  ]
+};
 
+// make an example out of this middleware ;)
 var exampleMiddle = function (req, res, next) {
 
   // create new engineer object
   var example = new engineer();
+  
+  // get the start time
+  var startMs = Date.now();
 
+  // running .make gives us some html to do with as we wish
   example.make(exampleOptions, data, function (err, html) {
     
     if (err) {
+
       return next(err, null);
+
     }
 
     if (html) {
+
+      // set `res.locals.table`
       res.locals.table = html;
+
+      // store end time to get our speed test.
+      var endMs = Date.now();
+
+      // find out how long it took...
+      console.log('It took ' + (endMs - startMs) + 'ms to render ' + data.length + ' rows.');
+
       return next();
+
+    } else {
+     
+      // using this pattern allows you to handle not having
+      // any html to return, and no error. Probably because you're
+      // missing something in your chain.
+
+      return next();
+    
     }
 
   });
@@ -39,10 +75,12 @@ var exampleMiddle = function (req, res, next) {
 
 var exampleRoute = function (req, res, next) {
 
+  // return res.render with our example.jade file
   return res.render('example');
 
 };
 
+// set app.get w/ some middleware and a route
 app.get('/', exampleMiddle, exampleRoute);
 
 server.listen(app.get('port'), function () {
--- src/index.coffee
+++ src/index.coffee
@@ -1,4 +1,10 @@
 ###
+# ENGINEER
+# I wrote this because I wanted a way for me to create server side tables with a jade file
+# and an object.
+###
+
+###
 # required modules
 ###
 
@@ -7,16 +13,12 @@ _ = require "lodash"
 fs = require "fs"
 path = require "path"
 
-###
-# ENGINEER
-# I wrote this because I wanted a way for me to create server side tables with a jade file
-# and an object.
-###
-
 engineer = (opts) ->
 
   # jade template file
-  @template = path.join __dirname, "templates", "..", "tables.jade"
+  @template = path.join __dirname, "..", "templates", "tables.jade"
+
+  @pretty = false
 
   # time to extend our object from opts
   if opts? then _.extend @, opts
@@ -28,13 +30,21 @@ engineer::make = (opts, input, fn) ->
   # define input - json/object tree
   @input = input
 
-  # keys and titles
-  @keys = []
-  @titles = []
-  @link = {}
+  # refactor to support array of objects so sorting
+  # is much easier.
+  @fields = []
+
+  # by default you can add as many options as you'd
+  # like and just do your rules with jade.
 
   # time to extend our object from opts
   if opts? then _.extend @, opts
+
+  # if we've passed fields  
+  if @fields.length > 0
+    @keys = _.pluck @fields, "key"
+    @titles = _.pluck @fields, "title"
+  else return fn "You must pass an array of objects in your options for fields", null
 
   self = @
 
@@ -42,9 +52,13 @@ engineer::make = (opts, input, fn) ->
   # if it does we're in business, lets 
   # render some htmlz
   fs.exists self.template, (exists) ->
-    if exists then jade.renderFile self.template, {engine: self, data: self.input}, (err, html) ->
+
+    if exists then jade.renderFile self.template, {engine: self, data: self.input, pretty: self.pretty}, (err, html) ->
+
       return if err? then fn err, null
+
       fn null, html
+
     else fn "You must provide a valid .jade file to render from", null
 
 
